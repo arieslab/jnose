a5147e0b4bff9296947ab062e3243c0b2dd3c17a
Tassio Virginio <tassiovirginio@gmail.com>
2020-03-15 00:45:43 -0300

    melhorias, adicionando novas páginas e funcionalidades

--- src/main/java/br/ufba/jnose/pages/EvolutionPage.java
+++ src/main/java/br/ufba/jnose/pages/EvolutionPage.java
@@ -4,11 +4,7 @@ import br.ufba.jnose.pages.base.BasePage;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
-import org.apache.wicket.request.http.WebResponse;
-
-import javax.servlet.http.Cookie;
 
 public class EvolutionPage extends BasePage {
     private static final long serialVersionUID = 1L;
