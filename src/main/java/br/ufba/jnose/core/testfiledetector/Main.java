e5de24a08ea16fa9c2fbaa45f904a58f97067a86
Tassio Virginio <tassiovirginio@gmail.com>
2020-04-23 22:07:13 -0300

    melhorias

--- src/main/java/br/ufba/jnose/core/testfiledetector/Main.java
+++ src/main/java/br/ufba/jnose/core/testfiledetector/Main.java
@@ -1,39 +1,16 @@
 package br.ufba.jnose.core.testfiledetector;
 
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import br.ufba.jnose.core.JNoseUtils;
-import br.ufba.jnose.core.evolution.Commit;
 import br.ufba.jnose.util.ResultsWriter;
 
 public class Main {
 
     private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
-
-//    public static List<String[]> start(String projectPath, Commit commit) {
-//
-//        List<JNoseUtils.TestClass> files = null;
-//        try {
-//            files = JNoseUtils.getFilesTest(projectPath);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        List<String[]> retorno = new ArrayList<>();
-//
-//        for (JNoseUtils.TestClass testClass : files) {
-//            DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_hhmmss");
-//            String dateString = dateFormat.format(commit.date);
-//            String[] linhaArray = {commit.id, commit.name, dateString, commit.msg, testClass.pathFile.toString(), testClass.numberLine.toString(), testClass.numberMethods.toString()};
-//            retorno.add(linhaArray);
-//        }
-//        return retorno;
-//    }
 
     public static String start(String projectPath, String projectName, String reportPath) throws IOException {
 
@@ -54,9 +31,5 @@ public class Main {
         }
         return resultsWriter.getOutputFile();
     }
-
-//    public static List<JNoseUtils.TestClass> start2(String projectPath, String projectName, String reportPath) throws IOException {
-//        return JNoseUtils.getFilesTest(projectPath);
-//    }
 
 }
