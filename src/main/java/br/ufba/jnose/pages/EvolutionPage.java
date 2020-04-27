57287312ab5250318b76de5cf4caa1d5af62ec66
Tassio Virginio <tassiovirginio@gmail.com>
2020-04-27 09:27:42 -0300

    melhorias

--- src/main/java/br/ufba/jnose/pages/EvolutionPage.java
+++ src/main/java/br/ufba/jnose/pages/EvolutionPage.java
@@ -23,7 +23,6 @@ import org.apache.wicket.util.time.Duration;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
