6647f2172e68df2468d9386f672fbd958c0dc88e
Tassio Virginio <tassiovirginio@gmail.com>
2020-09-28 10:25:39 -0300

    corrigindo bug da criação da pasta projects

--- src/main/java/br/ufba/jnose/WicketApplication.java
+++ src/main/java/br/ufba/jnose/WicketApplication.java
@@ -5,6 +5,8 @@ import br.ufba.jnose.pages.HomePage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
+import java.io.File;
+
 public class WicketApplication extends WebApplication {
 
     public static boolean COBERTURA_ON = false;
@@ -20,5 +22,9 @@ public class WicketApplication extends WebApplication {
         this.getMarkupSettings().setStripWicketTags(true);
         this.getDebugSettings().setAjaxDebugModeEnabled(false);
         CSVCore.load(this);
+        File file = new File("./projects");
+        if(!file.exists()){
+            file.mkdirs();
+        }
     }
 }
