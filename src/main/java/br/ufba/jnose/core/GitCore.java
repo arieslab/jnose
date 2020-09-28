bb0e567aaeaa51a7ea1ad25cc3e0aca637bbb242
Tassio Virginio <tassiovirginio@gmail.com>
2020-09-28 20:52:04 -0300

    substituindo um comando do git pelo jgit - tags

--- src/main/java/br/ufba/jnose/core/GitCore.java
+++ src/main/java/br/ufba/jnose/core/GitCore.java
@@ -4,9 +4,11 @@ import br.ufba.jnose.dto.Commit;
 import br.ufba.jnose.dto.Projeto;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.NoHeadException;
 import org.eclipse.jgit.lib.PersonIdent;
+import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
@@ -85,44 +87,84 @@ public class GitCore {
     public static ArrayList<Commit> gitTags(String pathExecute) {
 
         ArrayList<Commit> lista = new ArrayList<>();
-        int r = 0;
+
         try {
-            Process p = Runtime.getRuntime().exec("git tag", null, new File(pathExecute));
-            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String lineOut;
+            Git git = Git.open(new File(pathExecute));
 
-            while ((lineOut = input.readLine()) != null) {
-                String tagName = lineOut.trim();
-                Process detalhes = Runtime.getRuntime().exec("git show " + tagName, null, new File(pathExecute));
-                BufferedReader input2 = new BufferedReader(new InputStreamReader(detalhes.getInputStream()));
-                String commit = "";
-                String lineOut2;
+            List<Ref> call = git.tagList().call();
+            for (Ref ref : call) {
+                System.out.println("Tag: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName());
 
-                String id = "";
-                String name = "";
-                Date date = null;
-                String msg = "";
+                LogCommand log = git.log();
 
-                while ((lineOut2 = input2.readLine()) != null) {
-                    if (lineOut2.trim().contains("Tagger:")) {
-                        name = lineOut2.trim().replace("Tagger:", "").trim();
-                    }
-                    if (lineOut2.trim().contains("Date:")) {
-                        String dateString = lineOut2.trim().replace("Date:", "").trim();
-                        SimpleDateFormat formatter5 = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z", Locale.US);
-                        date = formatter5.parse(dateString);
-                    }
-                    if (lineOut2.trim().contains("commit ")) {
-                        id = lineOut2.trim().replace("commit ", "").trim();
-                    }
+                Ref peeledRef = git.getRepository().peel(ref);
+                if(peeledRef.getPeeledObjectId() != null) {
+                    log.add(peeledRef.getPeeledObjectId());
+                } else {
+                    log.add(ref.getObjectId());
                 }
-                lista.add(new Commit(id, name, date, msg, tagName));
+
+                Iterable<RevCommit> logs = log.call();
+                for (RevCommit revCommit : logs) {
+                    PersonIdent authorIdent = revCommit.getAuthorIdent();
+                    Date authorDate = authorIdent.getWhen();
+                    TimeZone authorTimeZone = authorIdent.getTimeZone();
+                    System.out.println(authorDate + " - Commit: " + revCommit + ", name: " + revCommit.getName() + ", id: " + revCommit.getId().getName());
+
+                    lista.add(new Commit(
+                            revCommit.getId().getName(),
+                            authorIdent.getName(),
+                            authorDate,
+                            revCommit.getFullMessage(),
+                            ref.getName()));
+                    break;
+                }
+
+
             }
-            input.close();
-            r = p.waitFor();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+//        int r = 0;
+//        try {
+//            Process p = Runtime.getRuntime().exec("git tag", null, new File(pathExecute));
+//            BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//            String lineOut;
+//
+//            while ((lineOut = input.readLine()) != null) {
+//                String tagName = lineOut.trim();
+//                Process detalhes = Runtime.getRuntime().exec("git show " + tagName, null, new File(pathExecute));
+//                BufferedReader input2 = new BufferedReader(new InputStreamReader(detalhes.getInputStream()));
+//                String commit = "";
+//                String lineOut2;
+//
+//                String id = "";
+//                String name = "";
+//                Date date = null;
+//                String msg = "";
+//
+//                while ((lineOut2 = input2.readLine()) != null) {
+//                    if (lineOut2.trim().contains("Tagger:")) {
+//                        name = lineOut2.trim().replace("Tagger:", "").trim();
+//                    }
+//                    if (lineOut2.trim().contains("Date:")) {
+//                        String dateString = lineOut2.trim().replace("Date:", "").trim();
+//                        SimpleDateFormat formatter5 = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z", Locale.US);
+//                        date = formatter5.parse(dateString);
+//                    }
+//                    if (lineOut2.trim().contains("commit ")) {
+//                        id = lineOut2.trim().replace("commit ", "").trim();
+//                    }
+//                }
+//                lista.add(new Commit(id, name, date, msg, tagName));
+//            }
+//            input.close();
+//            r = p.waitFor();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
         return lista;
     }
 
