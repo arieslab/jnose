e2b50a3b38f4afd3c1aed76f7e4d08730b1b249b
Tassio Virginio <tassiovirginio@gmail.com>
2020-10-03 16:21:18 -0300

    add cores nas celulas, quando estão com 0 e quando estão diferentes de zero

--- src/main/java/br/ufba/jnose/pages/ResultPage.java
+++ src/main/java/br/ufba/jnose/pages/ResultPage.java
@@ -2,7 +2,9 @@ package br.ufba.jnose.pages;
 
 import br.ufba.jnose.core.CSVCore;
 import br.ufba.jnose.core.JNoseCore;
+import br.ufba.jnose.core.testsmelldetector.testsmell.Util;
 import br.ufba.jnose.pages.base.ImprimirPage;
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.DownloadLink;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -42,7 +44,18 @@ public class ResultPage extends ImprimirPage {
 
     private void lb(int numero, List<String> linha, ListItem<List<String>> item){
         if(linha.size() > numero) {
-            item.add(new Label("lb" + numero, linha.get(numero)));
+
+            Label lb = new Label("lb" + numero, linha.get(numero));
+            if(Util.isInt(linha.get(numero))){
+                Integer n = Integer.parseInt(linha.get(numero));
+                if(n != 0){
+                    lb.add(new AttributeModifier("style","background-color:#ffe6e6"));
+                }else{
+                    lb.add(new AttributeModifier("style","background-color:#e6ffee"));
+                }
+            }
+
+            item.add(lb);
         }else{
             item.add(new Label("lb" + numero).setVisible(false));
         }
