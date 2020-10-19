3820ba67e592c5f9aa690b661597b0f0004e8225
Tassio Virginio <tassiovirginio@gmail.com>
2020-10-19 19:15:42 -0300

    corrigindo bug

--- src/main/java/br/ufba/jnose/core/DBCore.java
+++ src/main/java/br/ufba/jnose/core/DBCore.java
@@ -2,17 +2,37 @@ package br.ufba.jnose.core;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DBCore {
 
     public static void load(){
+        Statement stmt = null;
+        int result = 0;
+
         try {
             Class.forName("org.hsqldb.jdbc.JDBCDriver" );
-            Connection c = DriverManager.getConnection("jdbc:hsqldb:file:db/jnose.db", "SA", "");
+            Connection con = DriverManager.getConnection("jdbc:hsqldb:file:db/jnose.db", "SA", "");
+            stmt = con.createStatement();
+
+            result = stmt.executeUpdate(
+                    "CREATE TABLE resultados ( " +
+                            "id INT NOT NULL, " +
+                            "projectName VARCHAR(100) NOT NULL, " +
+                            "name VARCHAR(100) NOT NULL, " +
+                            "pathFile VARCHAR(1000) NOT NULL, " +
+                            "productionFile VARCHAR(1000), " +
+                            "junitVersion VARCHAR(10) NOT NULL, " +
+                            "loc INT NOT NULL, " +
+                            "qtdMethods INT NOT NULL, " +
+                            "testSmellName VARCHAR(100) NOT NULL, " +
+                            "testSmellMethod VARCHAR(100) NOT NULL, " +
+                            "testSmellLineBegin INT NOT NULL, " +
+                            "testSmellLineEnd INT NOT NULL, " +
+                            "date DATE, " +
+                            "PRIMARY KEY (id)); ");
         } catch (Exception e) {
-            System.err.println("ERRO: falha ao carregar o driver JDBC do HSQLDB!");
             e.printStackTrace();
-            return;
         }
 
     }
